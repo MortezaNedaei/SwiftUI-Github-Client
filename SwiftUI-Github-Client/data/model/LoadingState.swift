7dc49482f5ff6e9cd97a681fb23fbebb1d4ab446
Morteza Nedaei <nedaei1375@gmail.com>
2021-02-09 18:49:22 +0330

    add State enum to show handle different states of Network connections

---
+++ SwiftUI-Github-Client/data/model/LoadingState.swift
@@ -0,0 +1,12 @@
+//
+//  LoadingState.swift
+//  SwiftUI-Github-Client
+//
+//  Created by MoNe on 11/20/1399 AP.
+//
+
+import Foundation
+
+enum LoadingState {
+    case loading, success, failed
+}
