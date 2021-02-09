0e67fc54929e7192e4347166265255bdac2ff470
Morteza Nedaei <nedaei1375@gmail.com>
2021-02-09 18:11:54 +0330

    Initial Commit

---
+++ SwiftUI-Github-Client/SwiftUI_Github_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Github_ClientApp.swift
+//  SwiftUI-Github-Client
+//
+//  Created by MoNe on 11/21/1399 AP.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Github_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
