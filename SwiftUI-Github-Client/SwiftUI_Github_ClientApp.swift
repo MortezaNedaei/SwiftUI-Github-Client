ca566b375352ffe6e800186c2ebdc3b79e09a38f
Morteza Nedaei <nedaei1375@gmail.com>
2021-02-09 18:31:49 +0330

    add base project

--- SwiftUI-Github-Client/SwiftUI_Github_ClientApp.swift
+++ SwiftUI-Github-Client/SwiftUI_Github_ClientApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_Github_ClientApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainContentView()
         }
     }
 }
